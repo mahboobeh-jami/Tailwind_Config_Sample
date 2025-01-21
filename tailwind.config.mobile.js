eb2040275abf588fec6b6698b282af2934d00c45
mahboobeh-jami <kmg.d101@gmail.com>
2025-01-21 12:32:26 +0330

    upload sample for design team

---
+++ tailwind.config.mobile.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './mobile/**/*.html',       // فقط فایل‌های موبایل
+    './mobile/assets/js/**/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E3A8A',
+        secondary: '#9333EA',
+      },
+    },
+  },
+  plugins: [],
+};
---
+++ tailwind.config.web.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './web/**/*.html',          // فقط فایل‌های وب
+    './web/assets/js/**/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF',
+        secondary: '#A21CAF',
+      },
+    },
+  },
+  plugins: [],
+};
